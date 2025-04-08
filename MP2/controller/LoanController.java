54f2521f7394606ac1fcf50c8f14a2ecc92f38da
Ravetes <10666020@ucn.dk>
2025-04-08 12:07:30 +0200

    lort

---
+++ MP2/controller/LoanController.java
@@ -0,0 +1,14 @@
+package MP2.controller;
+import MP2.model.Loan;
+
+
+public class LoanController
+{
+    
+    
+    public LoanController()
+    {
+    
+    }
+
+}
---
+++ MP2/model/Friend.java
@@ -0,0 +1,11 @@
+package MP2.model;
+
+public class Friend
+{
+   private int phone;
+    public Friend(int phone)
+    {
+       this.phone = phone;
+    }
+    
+}
