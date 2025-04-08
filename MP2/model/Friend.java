33869a031c47a1c7262994aa367720b25dbd0dd4
Ravetes <10666020@ucn.dk>
2025-04-08 13:30:44 +0200

    lort, piss OG cum nu :)

--- MP2/model/Friend.java
+++ MP2/model/Friend.java
@@ -2,10 +2,18 @@ package MP2.model;
 
 public class Friend
 {
-   private int phone;
-    public Friend(int phone)
-    {
-       this.phone = phone;
+   private String name;
+    private int phone;
+    private String address;
+    private int postalcode;
+    private String city;
+    
+    public Friend(String name, int phone, String address, int postalcode, String city) {
+        this.name = name;
+        this.phone = phone;
+        this.address = address;
+        this.postalcode = postalcode;
+        this.city = city;
     }
     
 }
--- MP2/model/LP.java
+++ MP2/model/LP.java
@@ -1,14 +1,20 @@
 package MP2.model;
 
-
 public class LP
 {
-  private LPCopy lpcopy;
-  
-    public LP()
+    private LPCopy lpcopy;
+    private String barcode;
+    private String title;
+    private String artist;
+    private String publicationDate;
+
+    public LP(String barcode, String title, String artist, String publicationDate, LPCopy lpcopy)
     {
-    
+        this.barcode = barcode;
+        this.title = title;
+        this.artist = artist;
+        this.publicationDate = publicationDate;
     }
 
-    }
+}
 
